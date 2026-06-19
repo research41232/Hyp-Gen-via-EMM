29905c0acee8bb9c2b47ded38192eab813888da0
research41232 <research41232@gmail.com>
2026-06-19 10:47:44 +0200

    Update SEM_model_target.py

--- PLS-SEM-extension/SEM_model_target.py
+++ PLS-SEM-extension/SEM_model_target.py
@@ -26,9 +26,9 @@ class SEMTarget(BaseTarget):
 
     statistic_types = (
         "size_sg",
+        "disjoint_ci_count",
+        "disjoint_ci_paths",
         "size_ds",
-        "Gof_sg",
-        "Gof_ds",
         "paths_model",
         "sig_paths_sg",
         "sig_paths_ds",
@@ -64,61 +64,71 @@ class SEMTarget(BaseTarget):
         mask = np.asarray(mask)
         sg_data = data.iloc[np.where(mask)[0]]
         size_ds = len(data)
+
         
+        if size_sg >= 20:
+            pls_sg = Plspm(sg_data, self.config, Scheme.PATH, 1000, 0.00000001, False)
+            model_sg = pls_sg.inner_model()
+            model_sg = model_sg.rename(columns={"p>|t|": "p", "std error": "std_e"})
+            
 
-        pls_sg = Plspm(sg_data, self.config, Scheme.PATH, 100, 0.00000001, False)
-        pls_ds = Plspm(data, self.config, Scheme.PATH, 100, 0.00000001, False)
+            pls_ds = Plspm(data, self.config, Scheme.PATH, 1000, 0.00000001, False)
+            model_ds = pls_ds.inner_model()
+            model_ds = model_ds.rename(columns={"p>|t|": "p_ds", "estimate": "estimate_ds", "std error": "std_e_ds"})
 
-        model_sg = pls_sg.inner_model()
-        model_sg = model_sg.rename(columns={"p>|t|": "p"})
 
-        model_ds = pls_ds.inner_model()
-        model_ds = model_ds.rename(columns={"p>|t|": "p_ds", "estimate": "estimate_ds"})
+            paths_model = len(model_ds)
 
-        Gof_sg = pls_sg.goodness_of_fit()
-        Gof_ds = pls_ds.goodness_of_fit()
+            sig_paths_sg = (model_sg["p"] <= 0.05).sum()
+            sig_paths_ds = (model_ds["p_ds"] <= 0.05).sum()
 
-        paths_model = len(model_ds)
+            pos_sig_paths_sg = ((model_sg["p"] <= 0.05) & (model_sg["estimate"] > 0)).sum()
+            pos_sig_paths_ds = ((model_ds["p_ds"] <= 0.05) & (model_ds["estimate_ds"] > 0)).sum()
 
-        sig_paths_sg = (model_sg["p"] <= 0.05).sum()
-        sig_paths_ds = (model_ds["p_ds"] <= 0.05).sum()
+            neg_sig_paths_sg = ((model_sg["p"] <= 0.05) & (model_sg["estimate"] < 0)).sum()
+            neg_sig_paths_ds = ((model_ds["p_ds"] <= 0.05) & (model_ds["estimate_ds"] < 0)).sum()
+            
 
-        pos_sig_paths_sg = ((model_sg["p"] <= 0.05) & (model_sg["estimate"] > 0)).sum()
-        pos_sig_paths_ds = ((model_ds["p_ds"] <= 0.05) & (model_ds["estimate_ds"] > 0)).sum()
+            joint_model = model_ds.copy()
+            joint_model[["estimate", "p", "std_e"]] = model_sg[["estimate", "p", "std_e"]]
+            joint_model["lower_ci"] = joint_model["estimate"] - 1.96 * joint_model["std_e"]
+            joint_model["upper_ci"] = joint_model["estimate"] + 1.96 * joint_model["std_e"]
+            joint_model["lower_ci_ds"] = joint_model["estimate_ds"] - 1.96 * joint_model["std_e_ds"]
+            joint_model["upper_ci_ds"] = joint_model["estimate_ds"] + 1.96 * joint_model["std_e_ds"]
+            changes_sig_sg = 0
+            flip_sign_sg = 0
+            new_sig_sg = 0
+            disjoint_ci_count = 0
+            disjoint_ci_paths = []
 
-        neg_sig_paths_sg = ((model_sg["p"] <= 0.05) & (model_sg["estimate"] < 0)).sum()
-        neg_sig_paths_ds = ((model_ds["p_ds"] <= 0.05) & (model_ds["estimate_ds"] < 0)).sum()
+            for _, row in joint_model.iterrows():
+                if ((row["p"] <= 0.05 and row["p_ds"] > 0.05) or
+                    (row["p"] > 0.05 and row["p_ds"] <= 0.05)):
+                    changes_sig_sg += 1
 
-        joint_model = model_ds.copy()
-        joint_model[["estimate", "p"]] = model_sg[["estimate", "p"]]
-        changes_sig_sg = 0
-        flip_sign_sg = 0
-        new_sig_sg = 0
+                if (row["p"] <= 0.05 and row["p_ds"] > 0.05):
+                    new_sig_sg+=1
 
-        for _, row in joint_model.iterrows():
-            if ((row["p"] <= 0.05 and row["p_ds"] > 0.05) or
-                (row["p"] > 0.05 and row["p_ds"] <= 0.05)):
-                changes_sig_sg += 1
+                if ((row["p"] <= 0.05 and row["p_ds"] <= 0.05) and
+                    (np.sign(row["estimate"]) * np.sign(row["estimate_ds"]) == -1)):
+                    flip_sign_sg += 1
 
-            if (row["p"] <= 0.05 and row["p_ds"] > 0.05):
-                new_sig_sg+=1
+                if (row["upper_ci"] < row["lower_ci_ds"] or row["upper_ci_ds"] < row["lower_ci"]):
+                    disjoint_ci_count += 1
+                    disjoint_ci_paths.append(row.name)
 
-            if ((row["p"] <= 0.05 and row["p_ds"] <= 0.05) and
-                (np.sign(row["estimate"]) * np.sign(row["estimate_ds"]) == -1)):
-                flip_sign_sg += 1
-
-        return(size_sg, size_ds, Gof_sg, Gof_ds, paths_model, sig_paths_sg, sig_paths_ds, pos_sig_paths_sg, 
+        return(size_sg, disjoint_ci_count, disjoint_ci_paths, size_ds, paths_model, sig_paths_sg, sig_paths_ds, pos_sig_paths_sg, 
                pos_sig_paths_ds, neg_sig_paths_sg, neg_sig_paths_ds, changes_sig_sg,new_sig_sg, flip_sign_sg)
 
     def calculate_statistics(self, subgroup, data, cached_statistics=None):
         stats = {}
-        (size_sg, size_ds, Gof_sg, Gof_ds, paths_model, sig_paths_sg, sig_paths_ds, pos_sig_paths_sg, 
+        (size_sg, disjoint_ci_count,disjoint_ci_paths, size_ds, paths_model, sig_paths_sg, sig_paths_ds, pos_sig_paths_sg, 
                pos_sig_paths_ds, neg_sig_paths_sg, neg_sig_paths_ds, changes_sig_sg, new_sig_sg, flip_sign_sg) = self.get_base_statistics(subgroup, data)
         
         stats["size_sg"] = size_sg
+        stats["disjoint_ci_count"] = disjoint_ci_count
+        stats["disjoint_ci_paths"] = disjoint_ci_paths
         stats["size_ds"] = size_ds
-        stats["Gof_sg"] = Gof_sg
-        stats["Gof_ds"] = Gof_ds
         stats["paths_model"] = paths_model
         stats["sig_paths_sg"] = sig_paths_sg
         stats["sig_paths_ds"] = sig_paths_ds
@@ -211,7 +221,95 @@ class SEMQF(AbstractInterestingnessMeasure):
     def optimistic_estimate(self, subgroup, target, data, statistics=None):
         statistics = self.ensure_statistics(subgroup, target, data, statistics)
         return statistics.quality
+
+class SEMQFEntropyCI(AbstractInterestingnessMeasure):
+    """
+    Rewards paths for which the SG path estimate confidence interval is completely non-overlapping with the global model path estimate confidence interval. 
+    Sg size moderated by entropy measure. Does not require min sg size to be defined. 
+    Only sg with >=20 elements are considered due to PLS SEM restrictions.
+    Requires z for CI. 
+    """
+    tpl = namedtuple("SEMQFEntropyCI_tpl", ["changes_sig_sg", "flip_sign_sg", "quality", "size_sg"])
+
+    def __init__(self, config, z):
+        self.config = config
+        self.z = z
+        self.has_constant_statistics = False
+        self.required_stat_attrs = self.tpl._fields
+        self.dataset_statistics = None
+
+    def calculate_constant_statistics(self, data, target):
+         size_dataset = len(data)
+         self.dataset_statistics = self.tpl(None, None, None, size_dataset)
+         self.has_constant_statistics = True
     
+    def calculate_statistics(self, subgroup, target, data, statistics=None):
+        cover_arr, size_sg = get_cover_array_and_size(subgroup, len(data), data)
+
+        mask = cover_arr.representation if hasattr(cover_arr, "representation") else cover_arr
+        mask = np.asarray(mask)
+        sg_data = data.iloc[np.where(mask)[0]]
+
+        if size_sg >= 20:
+            try:
+                pls_sg = Plspm(sg_data, self.config, Scheme.PATH, 1000, 0.00000001, False)
+                model_sg = pls_sg.inner_model()
+                model_sg = model_sg.rename(columns={"p>|t|": "p", "std error": "std_e"})
+                
+
+                pls_ds = Plspm(data, self.config, Scheme.PATH, 1000, 0.00000001, False)
+                model_ds = pls_ds.inner_model()
+                model_ds = model_ds.rename(columns={"p>|t|": "p_ds", "estimate": "estimate_ds", "std error": "std_e_ds"})
+                
+
+                joint_model = model_ds.copy()
+                joint_model[["estimate", "p", "std_e"]] = model_sg[["estimate", "p", "std_e"]]
+                joint_model["lower_ci"] = joint_model["estimate"] - self.z * joint_model["std_e"]
+                joint_model["upper_ci"] = joint_model["estimate"] + self.z * joint_model["std_e"]
+                joint_model["lower_ci_ds"] = joint_model["estimate_ds"] - self.z * joint_model["std_e_ds"]
+                joint_model["upper_ci_ds"] = joint_model["estimate_ds"] + self.z * joint_model["std_e_ds"]
+                disjoint_ci_count = 0
+                flip_sign_sg = 0
+
+                for _, row in joint_model.iterrows():
+                    if (row["upper_ci"] < row["lower_ci_ds"] or row["upper_ci_ds"] < row["lower_ci"]):
+                        disjoint_ci_count += 1
+
+                    if ((row["p"] <= 0.05 and row["p_ds"] <= 0.05) and
+                        (np.sign(row["estimate"]) * np.sign(row["estimate_ds"]) == -1)):
+                        flip_sign_sg += 1
+                
+                p = size_sg / len(data)
+                if (p == 0 or p==1):
+                    quality = 0
+                    entropy_size = 0
+                else:
+                    entropy_size = -p * np.log(p) - (1 - p) * np.log(1 - p)
+                    quality = entropy_size * disjoint_ci_count
+
+                
+            except Exception:
+                return self.tpl(0, 0, 0, size_sg)
+
+        else:
+            disjoint_ci_count = 0
+            flip_sign_sg = 0
+            quality = 0
+            entropy_size = 0
+
+        print("disjoint_ci_count = ", disjoint_ci_count)
+        print("entropy_size = ", entropy_size)
+        print("quality = ", quality)
+            
+        return self.tpl(disjoint_ci_count, flip_sign_sg, quality, size_sg)
+    
+    def evaluate(self, subgroup, target, data, statistics=None):
+        statistics = self.ensure_statistics(subgroup, target, data, statistics)
+        return statistics.quality
+
+    def optimistic_estimate(self, subgroup, target, data, statistics=None):
+        statistics = self.ensure_statistics(subgroup, target, data, statistics)
+        return statistics.quality
 
 class SEMQFNewSig(AbstractInterestingnessMeasure):
     """
